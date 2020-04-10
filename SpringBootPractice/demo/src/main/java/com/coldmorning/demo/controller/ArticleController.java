f10cc7bbe143d8d5854943be6aef2cc2fa6c9889
Yu-Linang Huang <yulianghuang360@gmail.com>
2020-04-10 17:22:07 +0800

    Better code

--- SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/ArticleController.java
+++ SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/ArticleController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.net.URI;
-import java.security.cert.PKIXRevocationChecker.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -22,25 +21,26 @@ import com.coldmorning.demo.controller.entity.Article;
  * ArticleWorldController
  */
 @RestController
-@RequestMapping(value="/say")
+@RequestMapping(value="/Article")
+
 public class ArticleController {
 
     private List<Article> ArticleDB = new ArrayList<>();
-    @GetMapping(value="/Article1/{id}")
-    public Article sayArticle(@PathVariable("id") Integer id) {
+    @GetMapping(value="/GET1/{id}")
+    public Article sayArticle(@PathVariable("id") String id) {
         Article Article = new Article();
         Article.setId(id);
         return Article;
     }
 
-    @GetMapping(value="/Article2/{id}")
-    public ResponseEntity<Article> sayArticle2 (@PathVariable("id")  Integer id) {
+    @GetMapping(value="/GET2/{id}")
+    public ResponseEntity<Article> sayArticle2 (@PathVariable("id")  String id) {
         Article Article = new Article();
         Article.setId(id);
         return ResponseEntity.ok(Article);
     }
 
-    @GetMapping(value="/Article3/{id}")
+    @GetMapping(value="/GET3/{id}")
     public ResponseEntity<Article> sayArticle3 (@PathVariable("id")  String id) {
         Optional<Article> ArticleOp = ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
         if(ArticleOp.isPresent()){
@@ -49,15 +49,19 @@ public class ArticleController {
         }
         return ResponseEntity.notFound().build();
     } 
-    @PostMapping(value="/Article1")
+    @PostMapping(value="/POST1/")
     public ResponseEntity<Article> createArtice(@RequestBody Article reqest){
         boolean isIdExist = ArticleDB.stream().anyMatch(p->p.getId().equals(reqest.getId()));
+        
         if(isIdExist){
+
             return ResponseEntity.unprocessableEntity().build();
         }
         Article article = new Article();
         article.setId(reqest.getId());
+        article.setTitle("TEST");
         ArticleDB.add(article);
+    
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(article.getId()).toUri();
         return ResponseEntity.created(location).body(article);
     }
--- SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/entity/Article.java
+++ SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/entity/Article.java
@@ -1,29 +1,31 @@
 package com.coldmorning.demo.controller.entity;
 
 public class Article {
+    
     String title;
     String author;
-    Integer id;
+    String id;
     String floorNumber;
     StringBuilder articleContent;
-
-
     /**
      * @return the name
      */
-    public Integer getId() {
+    public String getId() {
         return this.id;
     }
 
-    public void setId( Integer id){
+    public void setId(String id){
         this.id = id;
     }
+
     public void setArticleContent(StringBuilder articleContent){
         this.articleContent = articleContent;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title){
         this.title = title;
     }
