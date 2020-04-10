22a12d32329eb96fed1c3118cd47c4e4153b78b8
Yu-Linang Huang <yulianghuang360@gmail.com>
2020-04-10 15:32:11 +0800

    Practice creating hello  controller

---
+++ SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/entity/Hello.java
@@ -0,0 +1,17 @@
+package com.coldmorning.demo.controller.entity;
+
+public class Hello {
+
+    String name;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
