c8b0c3ad41e22b52220525a2f505c82f03ab72b1
Yu-Linang Huang <yulianghuang360@gmail.com>
2020-04-11 22:50:42 +0800

    create Article DeleteMapping

--- SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/ArticleController.java
+++ SpringBootPractice/demo/src/main/java/com/coldmorning/demo/controller/ArticleController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import com.coldmorning.demo.entity.Article;
 public class ArticleController {
 
     private List<Article> ArticleDB = new ArrayList<>();
+
     @GetMapping(value="/GET1/{id}")
     public Article getArticle(@PathVariable("id") String id) {
         Article Article = new Article();
@@ -43,17 +45,18 @@ public class ArticleController {
 
     @GetMapping(value="/GET3/{id}")
     public ResponseEntity<Article> getArticle3 (@PathVariable("id")  String id) {
-        Optional<Article> ArticleOp = ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
-        if(ArticleOp.isPresent()){
-            Article Article = ArticleOp.get();
+        Optional<Article> articleOp = ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
+        if(articleOp.isPresent()){
+            Article Article = articleOp.get();
             return ResponseEntity.ok().body(Article);
         }
         return ResponseEntity.notFound().build();
     } 
+
     @PostMapping(value="/POST1/")
     public ResponseEntity<Article> createArtice(@RequestBody Article reqest){
-        boolean isIdPresent = ArticleDB.stream().anyMatch(p->p.getId().equals(reqest.getId()));
-        
+        boolean isIdPresent = ArticleDB.stream().anyMatch(p->p.getId().equals(reqest.getId())); 
+
         if(isIdPresent){
 
             return ResponseEntity.unprocessableEntity().build();
@@ -62,28 +65,36 @@ public class ArticleController {
         article.setId(reqest.getId());
         article.setTitle(reqest.getTitle());
         article.setArticleContent(reqest.getArticleContent());
-    
         ArticleDB.add(article);
     
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(article.getId()).toUri();
         return ResponseEntity.created(location).body(article);
     }
+
     @PutMapping(value="/PUT1/{id}")
     public ResponseEntity<Article> updateArticle(@PathVariable("id") String id, @RequestBody Article request){
-        Optional<Article> ArticleOp= ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
-        if(!ArticleOp.isPresent()){
+        Optional<Article> articleOp= ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
+        if(!articleOp.isPresent()){
             return ResponseEntity.notFound().build();
         }
         Article article = new Article();
-        Article OriArticle = ArticleOp.get();
+        Article OriArticle = articleOp.get();
         article.setId(OriArticle.getId());
         article.setTitle(request.getTitle());
         article.setArticleContent(request.getArticleContent());
         ArticleDB.add(ArticleDB.indexOf(OriArticle), article);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(article.getId()).toUri();
-
-
         return ResponseEntity.created(location).body(article);
     }
+
+    @DeleteMapping(value="/DELETE1/{id}")
+    public ResponseEntity<Article> deleteArticle(@PathVariable("id") String id){
+        Optional<Article> articleOp = ArticleDB.stream().filter(p->p.getId().equals(id)).findFirst();
+        if(articleOp.isPresent()){
+            ArticleDB.remove(articleOp.get());      
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
